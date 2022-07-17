b3a5226f96a7f88f2a1186e0521b8de5d398b31c
MasonWang025 <mason.wang0025@gmail.com>
2022-07-16 21:15:07 -0700

    update tailwind config

---
+++ src/data/routes.ts
@@ -0,0 +1,16 @@
+const routes = [
+  { href: "/", name: "Home" },
+  { href: "/playground", name: "Playground" },
+  {
+    name: "Other",
+    dropRoutes: [
+      { href: "https://oval.cs.stanford.edu/", name: "Stanford OVAL" },
+      {
+        href: "https://genie.stanford.edu/",
+        name: "Genie",
+      },
+    ],
+  },
+];
+
+export default routes;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   purge: [
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
     ,
-    "./data/**/*.{js,jsx,ts,tsx}",
+    "./src/data/**/*.{js,jsx,ts,tsx}",
   ],
   darkMode: false, // or 'media' or 'class'
   corePlugins: {
