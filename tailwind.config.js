3c5f3ba75adffe1e70d028c6fdc5cc15f8219586
MasonWang025 <mason.wang0025@gmail.com>
2022-07-16 20:22:45 -0700

    theme and css added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,43 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+  purge: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    ,
+    "./data/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: false, // or 'media' or 'class'
+  corePlugins: {
+    ringWidth: false,
+    outline: false,
+  },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        noora: {
+          primary: {
+            main: "#6940b6",
+            light: "#7447c9",
+            dark: "#5b379e",
+          },
+          secondary: {
+            main: "#0b122b",
+            bright: "#101a40",
+            gray: "#141c3b",
+            grayer: "#1a213b",
+            dark: "#050d2b",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      cursor: ["disabled"],
+    },
   },
   style: {
     postcss: {
       plugins: [require('tailwindcss'), require('autoprefixer')],
     },
   },
-  plugins: [],
-}
+};
