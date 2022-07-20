d036168b9d0f45a1d9b0018ab21ea41d1d293865
MasonWang025 <mason.wang0025@gmail.com>
2022-07-20 00:55:16 -0700

    noora header for chat

--- src/components/playground/practice/Practice.tsx
+++ src/components/playground/practice/Practice.tsx
@@ -25,10 +25,13 @@ export default function Practice() {
     <div className="bg-gray-100">
       <div className="pt-16 container">
         <div className="max-w-2xl mx-auto py-4">
-          <div
-            className="bg-white rounded-t-md border-2 border-b-0 border-gray-400 p-2"
-            id="chat"
-          >
+          <div className="w-full bg-white p-2 pt-3 border-2 border-gray-400 rounded-t-md">
+            <div className="h-16 w-16 mx-auto rounded-full bg-gradient-to-br from-noora-primary-light via-noora-primary-dark to-noora-primary-dark2"></div>
+            <div className="text-2xl text-center font-bold text-noora-primary-dark2">
+              Noora
+            </div>
+          </div>
+          <div className="bg-white border-x-2 border-gray-400 p-2" id="chat">
             <ul>
               {history.value.map((message: any, i: number) => (
                 <li key={i}>
