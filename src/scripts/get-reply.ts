5cf0db745d0a05dd060f2b09378434ffea132d6e
MasonWang025 <mason.wang0025@gmail.com>
2022-07-20 16:40:05 -0700

    statements returned when prompted

--- src/scripts/get-reply.ts
+++ src/scripts/get-reply.ts
@@ -1,4 +1,11 @@
 import { v4 as uuidv4 } from "uuid";
+import general_statements from "../data/module_statements/general";
+import work_statements from "../data/module_statements/work";
+
+const module_statements = {
+  general: general_statements,
+  work: work_statements,
+};
 
 export default async function getReply(
   message: string,
@@ -17,7 +24,7 @@ export default async function getReply(
 
   let reply = "Oops! Something went wrong.";
   console.log(command);
-  if (command == "get-statement") reply = await getStatement(delay);
+  if (command == "get-statement") reply = await getStatement(convoState, delay);
 
   let replyObj = {
     id: id,
@@ -30,12 +37,29 @@ export default async function getReply(
   historyQueue.setValue([...historyQueue.value, replyObj]);
 }
 
-async function getStatement(delay: number) {
-  await timeout(delay);
-  return "I read a really good book!";
+async function getStatement(convoState: any, delay: number) {
+  // choose module
+  const modules = convoState.value.modules.filter((m: any) => m.active);
+  const module: string = getRandomItem(modules).title;
+  console.log("Selected module: " + module);
+
+  // choose statement
+  let statement = getRandomItem(
+    module_statements[module as keyof typeof module_statements]
+  );
+  convoState.setValue({ ...convoState.value, statement: statement });
+  console.log("Selected statement: ");
+  console.log(statement);
+
+  // await timeout(delay);
+  return statement[1];
 }
 
 function timeout(ms: number) {
   // for testing purposes
   return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function getRandomItem(items: any) {
+  return items[Math.floor(Math.random() * items.length)];
 }
