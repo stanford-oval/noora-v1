7179033d9aee53306d68ac8f0698d302238832fb
MasonWang025 <mason.wang0025@gmail.com>
2022-08-02 15:15:50 -0700

    done implementing new prompts

--- src/scripts/generate-evaluation-prompt.ts
+++ src/scripts/generate-evaluation-prompt.ts
@@ -1,47 +1,13 @@
-import { selectAttitudes, getFewShotExamples } from "./v6_utils";
+import evalPrompts from "../data/prompts/noora-eval-prompt";
 
 export default function formPrompt(
   statement: string,
   category: string,
   reply: string
 ) {
-  const attitudes = selectAttitudes();
+  let prompt = evalPrompts[category.split("/")[1] as keyof typeof evalPrompts];
 
-  // console.log(attitudes);
-
-  const fewShotExamples = getFewShotExamples(category.split("/")[1], attitudes);
-  // console.log(fewShotExamples);
-
-  const prompt = formEvalPrompt(statement, reply, fewShotExamples);
+  prompt += `\n\nYou said, "${statement}"\n(1) ${reply}\nFeedback:`;
 
   return prompt;
-}
-
-function formEvalPrompt(
-  statement: string,
-  reply: string,
-  fewShotExamples: any[]
-) {
-  let prompt = "";
-
-  fewShotExamples.forEach((ex) => {
-    console.log(ex);
-    prompt += `You said, "${ex["statement"]}"\n`;
-    console.log("attitudes:", Object.keys(ex["replies"]));
-    Object.values(ex["replies"]).forEach((reply: any, idx: number) => {
-      prompt += `(${idx + 1}) I replied, "${reply["reply"]}"\n`;
-      prompt += `Feedback: ${capFirst(reply["rating"].trim())} reply. ${
-        reply["explanation"]
-      }\n`;
-    });
-    prompt += "\n";
-  });
-
-  // unseen statement
-  prompt += `You said, "${statement}"\n(1) ${reply}\nFeedback:`;
-  return prompt;
-}
-
-function capFirst(str: string) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
 }
