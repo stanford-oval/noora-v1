45c1eabb26c31cf93ae02d253a7f60a92c8112b6
Ethan <ethanytchang@gmail.com>
2022-08-05 12:32:54 -0700

    continuous speech working

--- src/components/global/utility/Microphone.tsx
+++ src/components/global/utility/Microphone.tsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMicrophone } from "@fortawesome/free-solid-svg-icons";
 
-import { ResultReason } from "microsoft-cognitiveservices-speech-sdk";
+import {
+  SpeechRecognizer,
+  ResultReason,
+  SpeechConfig,
+  AudioConfig,
+} from "microsoft-cognitiveservices-speech-sdk";
 const speechsdk = require("microsoft-cognitiveservices-speech-sdk");
 
 import { getTokenOrRefresh } from "../../../scripts/token_util";
@@ -14,17 +19,34 @@ export default function Microphone({
   setText,
   currText,
 }: any) {
-  const microphoneHandler = () => {
+  const [recog, setRecog] = useState<SpeechRecognizer>();
+
+  // initialize recognizer
+  useEffect(() => {
+    if (recog) return;
+    const init = async () => {
+      setRecog(await initRecognizer());
+    };
+    init();
+  }, []);
+
+  const microphoneHandler = async (recognizer: any, mode: boolean) => {
     console.log("In Microphone handler");
-    sttFromMic(turn, setTurn, setText, currText);
+    if (mode) await sttFromMic(turn, setTurn, setText, currText, recognizer);
+    else stopSttFromMic(turn, setTurn, recog);
   };
 
   return (
     <button
       type="button"
-      onClick={(e: any) => {
+      onMouseDown={(e: any) => {
         e.preventDefault();
-        microphoneHandler();
+        microphoneHandler(recog, true);
+      }}
+      onMouseUp={(e: any) => {
+        console.log("released");
+        e.preventDefault();
+        microphoneHandler(recog, false);
       }}
       disabled={turn.includes("read") || turn.includes("rate-reply")}
       className={className}
@@ -34,37 +56,67 @@ export default function Microphone({
   );
 }
 
-async function sttFromMic(
-  turn: string,
-  setTurn: any,
-  setText: any,
-  currText: string
-) {
+async function initRecognizer() {
   const tokenObj = await getTokenOrRefresh();
 
-  const speechConfig = speechsdk.SpeechConfig.fromAuthorizationToken(
+  const speechConfig = SpeechConfig.fromAuthorizationToken(
     tokenObj.authToken,
     tokenObj.region
   );
   speechConfig.speechRecognitionLanguage = "en-US";
 
-  const audioConfig = speechsdk.AudioConfig.fromDefaultMicrophoneInput();
-  const recognizer = new speechsdk.SpeechRecognizer(speechConfig, audioConfig);
+  const audioConfig = AudioConfig.fromDefaultMicrophoneInput();
+  const recognizer: SpeechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
 
-  if (turn.startsWith("user")) setTurn("user-answer-microphone")
+  recognizer.recognizing = function (s, e) {
+    var str = "(recognizing) Reason: " + ResultReason[e.result.reason] + " Text: " + e.result.text;
+    console.log(str);
+  };
+
+  return recognizer;
+}
+
+async function sttFromMic(
+  turn: string,
+  setTurn: any,
+  setText: any,
+  currText: string,
+  recognizer: SpeechRecognizer
+) {
+  if (turn.startsWith("user")) setTurn("user-answer-microphone");
   else return;
 
-  recognizer.recognizeOnceAsync((result: any) => {
-    setTurn(turn + "-edit");
-    let transcribed;
-    if (result.reason === ResultReason.RecognizedSpeech) {
-      transcribed = `${result.text}`;
-    } else return;
+  recognizer.startContinuousRecognitionAsync();
 
-    setText(
-      currText +
-      (currText.length > 0 && !currText.endsWith(" ") ? " " : "") +
-      transcribed
-    );
-  });
+  // (result: any) => {
+  //   let transcribed;
+  //   if (result.reason === ResultReason.RecognizedSpeech) {
+  //     transcribed = `${result.text}`;
+  //   } else return;
+
+  //   setText(
+  //     currText +
+  //       (currText.length > 0 && !currText.endsWith(" ") ? " " : "") +
+  //       transcribed
+  //   );
+  // }
+
+  // recognizer.recognizeOnceAsync((result: any) => {
+  //   setTurn(turn + "-edit");
+  //   let transcribed;
+  //   if (result.reason === ResultReason.RecognizedSpeech) {
+  //     transcribed = `${result.text}`;
+  //   } else return;
+
+  //   setText(
+  //     currText +
+  //       (currText.length > 0 && !currText.endsWith(" ") ? " " : "") +
+  //       transcribed
+  //   );
+  // });
+}
+
+async function stopSttFromMic(turn: any, setTurn: any, recognizer: any) {
+  setTurn(turn + "-edit");
+  recognizer.stopContinuousRecognitionAsync();
 }
