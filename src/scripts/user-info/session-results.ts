f010b256df8992965a3a5e6f2e03489d297019bf
MasonWang025 <mason.wang0025@gmail.com>
2022-08-08 14:41:47 -0700

    done writing parsed result to local storage

--- src/scripts/user-info/session-results.ts
+++ src/scripts/user-info/session-results.ts
@@ -1,7 +1,26 @@
 import { writeResultToLocal } from "./local-storage-utils";
 
 export function saveSessionResult(progressObj: any) {
-    let result = progressObj // temp
+    let result = {
+        timeCompleted: Date.now(),
+        scores: {
+            // [correct, attempted]
+            // positive, neutral, negative
+            total: [0, 0]
+        }
+    }
+
+    progressObj.forEach((problem: any) => {
+        let sentiment = problem.statementCategory.split("/")[1]
+        let sentimentKey = sentiment as keyof typeof result.scores
+        if (!result.scores[sentimentKey])
+            result.scores[sentimentKey] = [0, 0]
+
+        result.scores[sentimentKey][1]++;
+        result.scores.total[1]++;
+        result.scores[sentimentKey][0] += problem.goodAnswer ? 1 : 0;
+        result.scores.total[0] += problem.goodAnswer ? 1 : 0;
+    })
 
     console.log("### Saving to progress: ", result)
     writeResultToLocal(result)
