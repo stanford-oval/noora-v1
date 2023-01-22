6705c4c222c8544b57e7318d9138977cb299b4f7
nathanchi <nathanandrewchi@gmail.com>
2023-01-22 00:54:31 -0800

    Updated modules.ts

--- src/data/modules.ts
+++ src/data/modules.ts
@@ -17,13 +17,13 @@ const modules = {
     desc: "In this module, you will learn how to respond appropriately to a variety of topics including family, weather, and school. ",
     selectedDesc:
       "Noora is your friend and you will practice replying to statements of a variety of general topics.",
-    example: {
-      statement: "I had a great vacation.",
-      reply: "That’s great. Where did you go?",
-      sentiment: "Positive",
-      explanation:
-        "Your reply shows that you are happy for me.",
-    },
+    // example: {
+    //   statement: "I had a great vacation.",
+    //   reply: "That’s great. Where did you go?",
+    //   sentiment: "Positive",
+    //   explanation:
+    //     "Your reply shows that you are happy for me.",
+    // },
     icon: ChatAltIcon,
   },
   work: {
@@ -32,13 +32,13 @@ const modules = {
     desc: "In this module, you will learn how to respond appropriately to a variety of work-related topics such as promotions and job updates.",
     selectedDesc:
       "Noora is your co-worker and you will practice replying to statements you might hear in a workplace.",
-    example: {
-      statement: "I’m way too busy right now.",
-      reply: "I’m sorry. Can I help you with anything?",
-      sentiment: "Negative",
-      explanation:
-        "Your reply shows that you understand and want to help me.",
-    },
+    // example: {
+    //   statement: "I’m way too busy right now.",
+    //   reply: "I’m sorry. Can I help you with anything?",
+    //   sentiment: "Negative",
+    //   explanation:
+    //     "Your reply shows that you understand and want to help me.",
+    // },
     icon: BriefcaseIcon,
   },
   relevantQuestions: {
