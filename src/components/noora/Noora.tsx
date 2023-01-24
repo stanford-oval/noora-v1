f94694cb7061de629a3eb254df396413fb8fd55b
nathanchi <nathanandrewchi@gmail.com>
2023-01-24 09:00:55 -0800

    Updated fixes for modules sidebar

--- src/components/noora/Noora.tsx
+++ src/components/noora/Noora.tsx
@@ -39,12 +39,13 @@ export default function Noora() {
               modules={(selectedModule.module == "all"
                 ? Object.values(modules).map((m: any) => {
                   if (m.module == "all") return;
-                  return { title: m.title, active: true, fixed: true };
+                  return { title: m.module, displayName: m.title, active: true, fixed: true };
                 })
                 : Object.values(modules).map((m: any) => {
                   if (m.module == "all") return;
                   return {
-                    title: m.title,
+                    title: m.module,
+                    displayName: m.title,
                     active: selectedModule.module == m.module,
                     fixed: true,
                   };
