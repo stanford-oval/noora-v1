417320e07d7b65453109d0a2dee4756ec0a03e4c
MasonWang025 <mason.wang0025@gmail.com>
2023-04-17 14:00:42 -0700

    temporary helper function for gpt-4 use

--- src/scripts/gpt-3/Completion.tsx
+++ src/scripts/gpt-3/Completion.tsx
@@ -1,12 +1,35 @@
 export default async function Completion(parameters: CompletionParameters) {
+  const requestBody = convertPromptToChat(parameters);
+
   let reply = await fetch("/api/openai", {
     method: "POST",
-    body: JSON.stringify(parameters),
+    body: JSON.stringify(requestBody),
   }).then((res) => res.json());
 
   if (reply) reply = reply.text;
 
   return reply;
+}
+
+function convertPromptToChat(parameters: CompletionParameters) {
+  return {
+    model: "gpt-4",
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant.",
+      },
+      {
+        role: "user",
+        content: parameters.prompt,
+      },
+    ],
+    temperature: parameters.temperature,
+    max_tokens: parameters.max_tokens,
+    frequency_penalty: parameters.frequency_penalty,
+    presence_penalty: parameters.presence_penalty,
+    stop: parameters.stop,
+  };
 }
 
 type CompletionParameters = {
